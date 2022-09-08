8511aef114ac6e4e96194c6f5e533e4ab6d5c61b
davydovMikhail <davydov27977@gmail.com>
2022-09-08 13:57:24 +0700

    upgradeable thats all

--- test/token.test.ts.ts
+++ test/token.test.ts.ts
@@ -3,47 +3,48 @@ import { anyValue } from "@nomicfoundation/hardhat-chai-matchers/withArgs";
 import { expect } from "chai";
 import { ethers, upgrades } from "hardhat";
 
-// describe("Upgradeable(Transparent) token", function () {
-  // async function dep() {
-  //   const [ deployer ] = await ethers.getSigners();
+describe("Upgradeable(Transparent) token", function () {
+  async function dep() {
+    const [ deployer, user ] = await ethers.getSigners();
  
-  //   const NFTFactory = await ethers.getContractFactory("MyToken");
-  //   const token = await upgrades.deployProxy(NFTFactory, [], {   // первым аргументом передаем фабрику которая будет использована для деплоя, вторым аргументы деплоя которые передаются как бы в конструктор, которые будут переданы в функцию initialize, которая будет вызвана сразу после деплоя
-  //     initializer: 'initialize', // здесь указывается функция которая будет вызвана после деплоя
-  //   })
-  //   // token - это proxy контракт, который делает delegatecall на имплементацию
+    const NFTFactory = await ethers.getContractFactory("MyToken");
+    const token = await upgrades.deployProxy(NFTFactory, [], {   // первым аргументом передаем фабрику которая будет использована для деплоя, вторым аргументы деплоя которые передаются как бы в конструктор, которые будут переданы в функцию initialize, которая будет вызвана сразу после деплоя
+      initializer: 'initialize', // здесь указывается функция которая будет вызвана после деплоя
+    })
+    // token - это proxy контракт, который делает delegatecall на имплементацию
 
-  //   await token.deployed();
+    await token.deployed();
 
-  //   return { token, deployer };
-  // }
+    return { token, deployer, user };
+  }
   
 
-  // it("works", async function () {
-  //    const { token, deployer } = await loadFixture(dep);
+  it("works", async function () {
+     const { token, deployer, user } = await loadFixture(dep);
 
-  //    const mintTx = await token.safeMint(deployer.address, "qwertythb");
-  //    await mintTx.wait();
+     const mintTx = await token.safeMint(deployer.address, "qwertythb");
+     await mintTx.wait();
 
-  //    expect(await token.balanceOf(deployer.address)).to.eq(1);
+     expect(await token.balanceOf(deployer.address)).to.eq(1);
 
-  //    const NFTFactoryV2 = await ethers.getContractFactory("MyTokenV2");
-  //    const token2 = await upgrades.upgradeProxy(token.address, NFTFactoryV2);
+     const NFTFactoryV2 = await ethers.getContractFactory("MyTokenV2");
 
-  //    expect(token.address).to.eq(token2.address);
+     const token2 = await upgrades.upgradeProxy(token.address, NFTFactoryV2);
+
+     expect(token.address).to.eq(token2.address);
 
 
-  //    expect(await token.balanceOf(deployer.address)).to.eq(1);
+     expect(await token.balanceOf(deployer.address)).to.eq(1);
 
-  //    expect(await token2.demo()).to.eq(true);
+     expect(await token2.demo()).to.eq(true);
 
- 
-  // });
-// });
+  });
+});
 
-describe("Upgradeable(UUPS) token", function () {
+describe("Upgradeable(UUPS) token", async  function () {
+  // const [ deployer, user ] = await ethers.getSigners();
   async function dep1() {
-    const [ deployer ] = await ethers.getSigners();
+    const [ deployer, user ] = await ethers.getSigners();
  
     const NFTFactory = await ethers.getContractFactory("MyTokenUUPS");
     const token = await upgrades.deployProxy(NFTFactory, [], {   // первым аргументом передаем фабрику которая будет использована для деплоя, вторым аргументы деплоя которые передаются как бы в конструктор, которые будут переданы в функцию initialize, которая будет вызвана сразу после деплоя
@@ -54,28 +55,30 @@ describe("Upgradeable(UUPS) token", function () {
 
     await token.deployed();
 
-    return { token, deployer };
+    return { token, deployer, user };
   }
   
 
   it("works", async function () {
-     const { token, deployer } = await loadFixture(dep1);
+    const { token, deployer, user } = await loadFixture(dep1);
 
-     const mintTx = await token.safeMint(deployer.address, "qwertythb");
-     await mintTx.wait();
+    const mintTx = await token.safeMint(deployer.address, "qwertythb");
+    await mintTx.wait();
 
-     expect(await token.balanceOf(deployer.address)).to.eq(1);
+    expect(await token.balanceOf(deployer.address)).to.eq(1);
 
-     const NFTFactoryV2 = await ethers.getContractFactory("MyTokenUUPSv2");
-     const token2 = await upgrades.upgradeProxy(token.address, NFTFactoryV2);
+    let NFTFactoryV2 = await ethers.getContractFactory("MyTokenUUPSv2", user);
 
-     expect(token.address).to.eq(token2.address);
+    await expect(upgrades.upgradeProxy(token.address, NFTFactoryV2)).to.be.revertedWith("Ownable: caller is not the owner");
 
+    NFTFactoryV2 = await ethers.getContractFactory("MyTokenUUPSv2");
 
-     expect(await token2.balanceOf(deployer.address)).to.eq(1);
+    const token2 = await upgrades.upgradeProxy(token.address, NFTFactoryV2);
 
-     expect(await token2.demo()).to.eq(true);
+    expect(token.address).to.eq(token2.address);
 
- 
+    expect(await token2.balanceOf(deployer.address)).to.eq(1);
+
+    expect(await token2.demo()).to.eq(true); 
   });
 });
